f2fe5ceccfacd095d0fa744004c62e6f1d457d35
sandrusmb <sandrusmb@gmail.com>
2020-06-16 19:13:02 +0200

    Add setState

--- src/components/App.js
+++ src/components/App.js
@@ -7,22 +7,33 @@ import Companies from './Companies';
 
 class App extends React.Component {
   state = {
-    companies: []
+    companies: [],
+    amount: 1,
+    currency: "BTC"
   }
 
   getData = (ev) => {
+
     ev.preventDefault();
+    const amount = ev.target.elements[0];
+    console.log(amount.value);
+    const { currency } = ev.target.elements;
+
+
+    console.log(currency.value);
     fetch("https://compare.monedero.com/api/getPrice")
       .then(response => {
         return (response.json());
       })
       .then(response => {
-        this.setState({ companies: response })
+        this.setState({
+          companies: response,
+          amount: amount.value,
+          currency: currency.value
+        })
       })
 
   }
-
-
 
   render() {
 
@@ -30,7 +41,7 @@ class App extends React.Component {
       <div>
         <p>Conversor</p>
         <Form getData={this.getData} />
-        <Companies companies={this.state.companies} />
+        <Companies companies={this.state.companies} amount={this.state.amount} currency={this.state.currency} />
       </div>
     )
   }
--- src/components/Companies.js
+++ src/components/Companies.js
@@ -3,30 +3,31 @@ import ".././stylesheets/companies.scss";
 
 const Companies = (props) => {
     let info = props.companies;
+    let number = props.amount;
+    let name = props.currency;
+    console.log(number);
     let companiesKeys = Object.keys(info);
     let companiesValues = Object.values(info);
-    let i;
-    let j;
-    for (let i = 0; i < companiesKeys.length; i++) {
-        console.log(companiesKeys[i]);
-    }
-    for (let j = 0; j < companiesValues.length; j++) {
-        console.log(companiesValues[j]);
-    }
+
     return (
         <div className="container">
             <div className="container_keys">
                 {companiesKeys.map(companyKey => {
                     return (
-                        <p key={i}>{companyKey}</p>
+                        <p>{companyKey}</p>
                     )
                 })}
             </div>
             <div className="container_values">
                 {companiesValues.map(companyValue => {
-                    return (
-                        <p key={j}>{companyValue}</p>
-                    )
+
+                    if (name === "BTC") {
+                        return <p>{companyValue * number}</p>
+                    } else {
+                        return <p>{companyValue / number}</p>
+                    }
+
+
                 })}
             </div>
         </div>
